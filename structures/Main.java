cddca77261a786190f06d467840865f88fafb27f
Gonzalo Espeche <espeche.gonzalo@gmail.com>
2021-11-17 12:12:58 +0100

    se agrega Main.java

---
+++ structures/Main.java
@@ -0,0 +1,50 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /**
+         * Scanner scanner = new Scanner(System.in);
+         * int myInt = scanner.nextInt();
+         * double myDouble = scanner.nextDouble();
+         * String myString = scanner.next();
+         */
+
+        /**
+         * System.out.print("imprime un mensaje sin un enter al final de la linea")
+         * System.out.println("imprime un mensaje y deja una nueva linea al final")
+         */
+    }
+
+    /**
+     * Declarar 2 variables numericas, e indicar cual es la mayor de las dos.
+     * Si las dos variables son iguales, tambien indicar
+     */
+    public static void equals() {
+
+    }
+
+    /**
+     * Pedir un numero al usuario e imprimir si el numero es divisible por dos (num % 2 == 0)
+     */
+    public static void divisible() {
+
+    }
+
+    /**
+     * Mostrar los numeros del 1 al 100 que sean multiplos de 3
+     */
+    public static void multiplos() {
+
+    }
+
+    /**
+     * 1. Escribir una contraseña y guardarla en un String
+     * 2. Pedir a usuario que ingrese el password por consola
+     * 3. Si el usuario ingresa una contraseña invalida, se le tiene que pedir que ingrese el password una vez mas
+     * 4. Si a los 3 intentos, el usuario sigue ingresando un pass invalido, terminar el programa
+     */
+    public static void password() {
+
+    }
+}
