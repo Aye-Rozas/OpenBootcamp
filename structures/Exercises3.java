944c8504eab5535b66f24f9bd9476679e15c2ff8
Ayelen Rozas <ayelen29rozas@gmail.com>
2023-05-22 21:22:42 +0200

    se agrega solucion a los primeros ejercicios

---
+++ structures/Exercises3.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Exercises3 {
+    public static void main(String[] args) {
+        System.out.println(suma(2,4,6));
+    }
+
+    public static int suma(int a, int b, int c) {
+        int total = a + b + c;
+        return total;
+    }
+}
+
+
---
+++ structures/coche.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class coche {
+//    {
+//        public int puertas = 0;
+//        public void SumarPuertas;
+//        this.puertas++;
+//
+//    }
+//
+//    ;
+//
+//    public static void main(String[] args) {
+//        coche miCoche = new coche();
+//        miCoche.SumarPuertas();
+//        miCoche.SumarPuertas();
+//        miCoche.SumarPuertas();
+//        miCoche.SumarPuertas();
+//        System.out.println(miCoche.puertas);
+//    }
+}
+
